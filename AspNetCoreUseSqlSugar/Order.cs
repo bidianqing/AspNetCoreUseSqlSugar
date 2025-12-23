16fd8599a0fa87528bf20c09f0dc66c59dff4fec
bidianqing <bidianqing@qq.com>
2025-12-23 17:04:27 +0800

    *

--- AspNetCoreUseSqlSugar/Order.cs
+++ AspNetCoreUseSqlSugar/Order.cs
@@ -11,12 +11,17 @@ namespace AspNetCoreUseSqlSugar
         [SugarColumn(ColumnName = "order_price")]
         public decimal OrderPrice { get; set; }
 
+        [SugarColumn(ColumnName = "user_id")]
+        public Guid UserId { get; set; }
+
+        [SugarColumn(ColumnName = "is_deleted")]
         public bool IsDeleted { get; set; }
     }
 
     [SugarTable("tb_order_item")]
     public class OrderItem : BaseAuditableEntity
     {
+        [SugarColumn(ColumnName = "is_deleted")]
         public bool IsDeleted { get; set; }
 
         [SugarColumn(ColumnName = "order_id")]
@@ -25,11 +30,13 @@ namespace AspNetCoreUseSqlSugar
         [SugarColumn(ColumnName = "product_name")]
         public string ProductName { get; set; }
 
+        [SugarColumn(ColumnName = "unit")]
         public string Unit { get; set; }
 
         [SugarColumn(ColumnName = "unit_price")]
         public decimal UnitPrice { get; set; }
 
+        [SugarColumn(ColumnName = "quantity")]
         public int Quantity { get; set; }
     }
 }
